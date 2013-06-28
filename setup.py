0ea1edcc30df22b51759c5255741cb37606d3382
Jocelyn Read <jread@fullerton.edu>
2013-06-27 19:12:53 -0700

    test commit

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup
 
+# Everything I know about documentation I learned from
+# http://pythonhosted.org/an_example_pypi_project/
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
